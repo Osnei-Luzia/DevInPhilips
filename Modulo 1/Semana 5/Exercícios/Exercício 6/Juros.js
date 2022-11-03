768facdbc593eb4b7d6b9e2ad35a173bd54fdf8f
Osnei Luzia <osnei.luzia@gmail.com>
2022-11-03 00:10:11 -0300

    [M1S05] Ex 6 - Classe Juros

---
+++ Modulo 1/Semana 5/Exercícios/Exercício 6/Juros.js
@@ -0,0 +1,13 @@
+export default class Juros{
+    constructor(capitalInicial,taxaAplicada,tempo){
+        this.capitalInicial = capitalInicial
+        this.taxaAplicada = taxaAplicada
+        this.tempo = tempo
+    }
+    calcularJurosSimples(){
+        return this.capitalInicial * this.taxaAplicada * this.tempo
+    }
+    calcularJurosCompostos(){
+        return this.capitalInicial * (1+this.taxaAplicada) ** 120
+    }
+}
