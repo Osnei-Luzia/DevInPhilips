1bb9449fa0f052eacab9db706243f15af8ec030a
Osnei Luzia <osnei.luzia@gmail.com>
2022-11-26 19:17:07 -0300

    [M1S08] Ex 7 - Mover valores estáticos que estão no template para passar a olhar para o TS do componente.

--- Modulo 1/Semana 8/Projeto/src/app/app.component.ts
+++ Modulo 1/Semana 8/Projeto/src/app/app.component.ts
@@ -7,4 +7,22 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Projeto';
+  conteudo = [
+    {
+      titulo: "Titulo1",
+      subtitulo: "Subtitulo1",
+      data: new Date()
+    },
+    {
+      titulo: "Titulo2",
+      subtitulo: "Subtitulo2",
+      data: new Date()
+    },
+    {
+      titulo: "Titulo3",
+      subtitulo: "Subtitulo3",
+      data: new Date()
+    }
+  ]
 }
+
