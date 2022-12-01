08847c4e02ab7f3c96cb50efefee8f9a5bd6f439
Osnei Luzia <osnei.luzia@gmail.com>
2022-12-01 20:33:17 -0300

    [M1S09] Ex 5 - Property Binding (Parte 2)01~

--- Modulo 1/Semana 9/Exercícios/projetoAngular/src/app/item-card/item-card.component.ts
+++ Modulo 1/Semana 9/Exercícios/projetoAngular/src/app/item-card/item-card.component.ts
@@ -6,8 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./item-card.component.scss']
 })
 export class ItemCardComponent {
-  nome: String = "Nome"
-  // img: String = "src"
-  // ator: String = "Ator"
-  // descricao: String = "Descrição"
+  personagem: any = {
+    nome: 'Jon Snow',
+    imagem: 'https://tm.ibxk.com.br/2022/08/26/26174558850404.jpg?    ims=704x264',
+    nomeAtor: 'Kit Harington',
+    descricao:
+      'Ator britânico. É mais conhecido por interpretar Jon Snow na     série televisiva Game of Thrones da HBO. Meu marido, mas ele não sabe u.u',
+  };
 }
