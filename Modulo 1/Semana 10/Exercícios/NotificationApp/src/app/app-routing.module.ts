c63f3347e8c13c4c368cfa6912feda7a1bc434f8
Osnei Luzia <osnei.luzia@gmail.com>
2022-12-09 18:22:22 -0300

    [M1S10] Ex 6 - Configuração de rotas - Continuação

--- Modulo 1/Semana 10/Exercícios/NotificationApp/src/app/app-routing.module.ts
+++ Modulo 1/Semana 10/Exercícios/NotificationApp/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent},
+  {path:'home/:todos',component:HomeComponent},
+  {path:'home/:lidos',component:HomeComponent},
+  {path:'home/:nao-lidos',component:HomeComponent},
+  {path:'about',component:AboutComponent},
+  {path:'/',component:HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
