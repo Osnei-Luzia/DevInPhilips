4d6fc2e4356f2ad8c3a7c87b6684c2efc9bfdfd4
Osnei Luzia <osnei.luzia@gmail.com>
2022-12-09 19:02:25 -0300

    [M1S10] Ex 7 - Utilização de roteamento dinamico

--- Modulo 1/Semana 10/Exercícios/NotificationApp/src/app/app-routing.module.ts
+++ Modulo 1/Semana 10/Exercícios/NotificationApp/src/app/app-routing.module.ts
@@ -9,7 +9,7 @@ const routes: Routes = [
   {path:'home/:lidos',component:HomeComponent},
   {path:'home/:nao-lidos',component:HomeComponent},
   {path:'about',component:AboutComponent},
-  {path:'/',component:HomeComponent}
+  {path:'',component:HomeComponent}
 ];
 
 @NgModule({
