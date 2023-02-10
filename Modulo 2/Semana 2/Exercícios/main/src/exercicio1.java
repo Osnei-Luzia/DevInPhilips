7787c4beed66dc463c5ab851834786cd7898a16b
Osnei Luzia <osnei.luzia@gmail.com>
2023-02-10 16:54:26 -0300

    Atividade 1

---
+++ Modulo 2/Semana 2/Exercícios/main/src/exercicio1.java
@@ -0,0 +1,8 @@
+public class exercicio1 {
+    public static void metodo() {
+        int[] arr = {56, 57, 58, 67, 99, 12, 11, 88};
+        System.out.println(arr[0]);
+        System.out.println(arr[7]);
+        System.out.println(arr[2]);
+    }
+}
