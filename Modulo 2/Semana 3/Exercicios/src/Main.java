1f30f4d247fdb1ca6b1d5f044161df2f0b51c101
Osnei Luzia <osnei.luzia@gmail.com>
2023-02-19 10:09:36 -0300

    [M2S03] Ex 2 - Cálculo do IMC

--- Modulo 2/Semana 3/Exercicios/src/Main.java
+++ Modulo 2/Semana 3/Exercicios/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-
+        clinica.calcIMC(52.0,1.73);
     }
 }
--- Modulo 2/Semana 3/Exercicios/src/clinica.java
+++ Modulo 2/Semana 3/Exercicios/src/clinica.java
@@ -4,4 +4,20 @@ public class clinica {
     String cnpj;
     String endereco;
     String id;
+
+    static Double calcIMC(Double peso, Double altura){
+        Double imc = peso/(altura*altura);
+        if(imc<25){
+            System.out.println("Peso Normal");
+        }else if(imc<30){
+            System.out.println("Excesso de Peso");
+        }else if(imc<35){
+            System.out.println("Obesidade Classe I");
+        }else if(imc<40){
+            System.out.println("Obesidade Classe II");
+        }else{
+            System.out.println("Obesidade Classe III");
+        }
+        return imc;
+    }
 }
