4330fbf6738bd13038f375957a1901f60dbe65b6
Osnei Luzia <osnei.luzia@gmail.com>
2023-03-27 12:25:19 -0300

    [M2S08] Ex 1 - Guarda empresas

---
+++ Modulo 2/Semana 8/Exercícios/ex1/Hello/src/main/java/servletss/NovaEmpresa.java
@@ -0,0 +1,46 @@
+package servletss;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NovaEmpresa
+ */
+@WebServlet("/NovaEmpresa")
+public class NovaEmpresa extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NovaEmpresa() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String empresa = request.getParameter("nome");
+		PrintWriter out = response.getWriter();
+		out.println("<h2>Empresa: "+empresa+"</h2>");
+		System.out.println("Empresa: "+empresa);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
