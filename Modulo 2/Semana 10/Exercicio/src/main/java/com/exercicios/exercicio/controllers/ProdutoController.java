9a37592de13d820cc8ab5be3a7bf83c06b379da6
Osnei Luzia <osnei.luzia@gmail.com>
2023-04-09 20:59:35 -0300

    [M2S10] Ex 3 - Criação dos endpoint Exibe Todos os Produtos e Criar novos produtos

--- Modulo 2/Semana 10/Exercicio/src/main/java/com/exercicios/exercicio/controllers/ProdutoController.java
+++ Modulo 2/Semana 10/Exercicio/src/main/java/com/exercicios/exercicio/controllers/ProdutoController.java
@@ -5,6 +5,7 @@ import com.exercicios.exercicio.controllers.services.ProdutoServices;
 import com.exercicios.exercicio.models.ProdutoEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,13 +21,23 @@ public class ProdutoController {
         this.produtoServices = produtoServices;
     }
 
-    @GetMapping
+    @GetMapping()
+    public String novoProduto(ProdutoDto produtoDto, Model model){
+        //model.addAttribute("novoProduto",produtoDto);
+        return "formularioProduto";
+    }
+    @PostMapping()
+    public String salvarProduto(@Validated ProdutoDto novoProduto, Model model){
+        produtoServices.salvarProduto(novoProduto);
+        return "redirect:produto/listarTodos";
+    }
+
+    /*@GetMapping
     public String mostrarProduto(Model model) {
         ProdutoDto produto = new ProdutoDto("nome","descricao",10.00);
-        produtoServices.salvarProduto(produto);
         model.addAttribute("produto", produto);
         return "produto";
-    }
+    }*/
 
     @GetMapping("/listarTodos")
     public String mostrarTodos(Model model){
@@ -35,8 +46,6 @@ public class ProdutoController {
         System.out.println(produtos);
         return "listaProdutos";
     }
-    @PostMapping("/criarNovo")
-    public void criarNovo(ProdutoDto produto, Model model){
 
-    }
+
 }
