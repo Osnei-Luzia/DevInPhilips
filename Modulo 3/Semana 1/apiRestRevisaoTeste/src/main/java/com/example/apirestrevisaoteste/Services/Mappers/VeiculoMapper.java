2a07aef59f997a92368bbe1633f789566dc0dceb
Osnei Luzia <osnei.luzia@gmail.com>
2023-05-22 10:08:08 -0300

    Correção de hibernate

--- Modulo 3/Semana 1/apiRestRevisaoTeste/src/main/java/com/example/apirestrevisaoteste/Services/Mappers/VeiculoMapper.java
+++ Modulo 3/Semana 1/apiRestRevisaoTeste/src/main/java/com/example/apirestrevisaoteste/Services/Mappers/VeiculoMapper.java
@@ -4,7 +4,8 @@ import com.example.apirestrevisaoteste.Controllers.dtos.RequestCriarVeiculo;
 import com.example.apirestrevisaoteste.Models.Veiculo;
 import org.mapstruct.*;
 
-@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)public interface VeiculoMapper {
+@Mapper(componentModel = MappingConstants.ComponentModel.SPRING, unmappedTargetPolicy = ReportingPolicy.IGNORE, nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+public interface VeiculoMapper {
     Veiculo map(RequestCriarVeiculo source);
     void update(@MappingTarget Veiculo target, RequestCriarVeiculo source);
 }
