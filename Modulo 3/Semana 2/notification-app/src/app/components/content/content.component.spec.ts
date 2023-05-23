14e2850a85ac09cff573b24f326a65d7b395b522
Osnei Luzia <osnei.luzia@gmail.com>
2023-05-23 13:45:45 -0300

    [M3S02] Ex 4 - ContentComponent: criação do teste para o método lerNotificacao

--- Modulo 3/Semana 2/notification-app/src/app/components/content/content.component.spec.ts
+++ Modulo 3/Semana 2/notification-app/src/app/components/content/content.component.spec.ts
@@ -2,6 +2,8 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppModule } from "src/app/app.module";
 import { ContentComponent } from "./content.component";
+import { of } from 'rxjs';
+import { NotificationService } from 'src/app/services/notification.service';
 
 describe('ContentComponent', () => {
 
@@ -9,11 +11,14 @@ describe('ContentComponent', () => {
     let fixture: ComponentFixture<ContentComponent>;
     //apelido para o mock do componente
     let component: ContentComponent;
+    //mock para serviço
+    let notificationService = jasmine.createSpyObj(NotificationService, ['editNotificationApi', 'getNotifications', 'removeNotification']);
 
     beforeEach(async () => {
         await TestBed.configureTestingModule({
             imports: [AppModule],
             declarations: [ContentComponent],
+            providers: [{ provide: NotificationService, useValue: notificationService }]
         })
             .compileComponents();
         fixture = TestBed.createComponent(ContentComponent);
@@ -27,9 +32,32 @@ describe('ContentComponent', () => {
 
         //teste unitario
         it('Should call carregarNotificacoes method with success', () => {
+
+            //observa o método
             spyOn(component, 'carregarNotificacoes');
             component.ngOnInit();
             expect(component.carregarNotificacoes).toHaveBeenCalled();
+        })
+    })
+    describe('AtualizarLista', () => {
+        it('lerNotificacao - Should call atualizarLista method with success', () => {
+            spyOn(component, 'atualizarLista');
+
+            //gera o mock para funcionamento do serviço
+            const notificacaoMock = {
+                aplicativo: '', titulo: '', descricao: '', tempoPublicacao: '',
+                imagem: '', lido: false, id: 1
+            };
+            //alteração do estado do mock anterior
+            const notificacaoEditadaMock = { ...notificacaoMock, lido: true };
+
+            //chamado de métodos do serviço
+            notificationService.editNotificationApi.and.returnValue(of({}));
+            //chamado de métodos do componente
+            component.lerNotificacao(notificacaoMock);
+
+            expect(notificationService.editNotificationApi).toHaveBeenCalledWith(notificacaoEditadaMock);
+            expect(component.atualizarLista).toHaveBeenCalled();
         })
     })
 })
