ddefcfd9b0b9d3a1b6316979ac3d984cdf941040
Osnei Luzia <osnei.luzia@gmail.com>
2023-05-23 13:38:51 -0300

    [M3S02] Ex 3 - ContentComponent: criação do teste para o ngOnInit

--- Modulo 3/Semana 2/notification-app/src/app/components/content/content.component.spec.ts
+++ Modulo 3/Semana 2/notification-app/src/app/components/content/content.component.spec.ts
@@ -1,17 +1,35 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { AppModule } from "src/app/app.module";
 import { ContentComponent } from "./content.component";
 
-beforeEach(async () => {
-    await TestBed.configureTestingModule({
-        imports: [AppModule],
-        declarations: [ContentComponent],
-    })
-        .compileComponents();
+describe('ContentComponent', () => {
 
-});
+    //mock do component
+    let fixture: ComponentFixture<ContentComponent>;
+    //apelido para o mock do componente
+    let component: ContentComponent;
 
-describe('submit', () => {
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            imports: [AppModule],
+            declarations: [ContentComponent],
+        })
+            .compileComponents();
+        fixture = TestBed.createComponent(ContentComponent);
+        component = fixture.componentInstance;
+    });
+
     it('Should create component', () => { expect(ContentComponent).toBeTruthy(); });
-});
+
+    //describe de novo teste
+    describe('ngOnInit', () => {
+
+        //teste unitario
+        it('Should call carregarNotificacoes method with success', () => {
+            spyOn(component, 'carregarNotificacoes');
+            component.ngOnInit();
+            expect(component.carregarNotificacoes).toHaveBeenCalled();
+        })
+    })
+})
